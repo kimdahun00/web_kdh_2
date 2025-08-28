c8371f0a9cfb1806d8a639e14540289c47a74793
kimdahun00 <dahunkim028@gmail.com>
2025-08-28 18:27:07 +0900

    fix: convert workspace from submodule to regular directory

---
+++ workspace/day04/js/const.js
@@ -0,0 +1,8 @@
+//2번 : 상수 const 키워드
+
+const  DATA = 10;
+console.log(DATA, typeof DATA, typeof(DATA));
+DATA = '2025년';
+// console.log(data, typeof data);
+//TypeError: Assignment to constant variable.
+
